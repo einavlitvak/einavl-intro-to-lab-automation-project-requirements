b271448aa9eae709822b3aa9a801ac081fc99f88
einavl <einav.litvak@weizmann.ac.il>
2026-07-12 15:56:38 +0300

    add exercises 2,3,4,5

---
+++ Project3-Measuring minimum delay in arduino/my_blink_ex3/my_blink.ino
@@ -0,0 +1,38 @@
+/*
+  my_blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. ledPin is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://docs.arduino.cc/hardware/
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/basics/Blink/
+*/
+int ledPin = 4;
+delay_time = 1000000;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin ledPin as an output.
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
+  delay(delay_time);                      // wait for a millisecond
+  digitalWrite(ledPin, LOW);   // change state of the LED by setting the pin to the LOW voltage level
+  delay(delay_time);                      // wait for a millisecond
+}
